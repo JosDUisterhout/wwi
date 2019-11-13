5ad33980a276f795c8d98c82486d10da1a58a908
leendert <s1149606@student.windesheim.nl>
2019-11-13 19:09:29 +0100

    navbar en wat styling

--- include.php
+++ include.php
@@ -2,3 +2,67 @@
 
 include('config.php');
 include('functies.php');
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+    <div class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#login" class="topnavright">Login</a>
+        <form method="get" class="zoek">
+            <div class="">
+                <input type="text" class="zoeken" name="zoek" placeholder="Zoeken" />
+                <button type="submit" name="submit" class="zoekbtn">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </form>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+
+    <form method="get" class="mobielzoek">
+        <input type="text" class="zoeken" name="zoek" placeholder="Zoeken" />
+        <button type="submit" name="submit" class="zoekbtn">
+            <i class="fa fa-search"></i>
+        </button>
+    </form>
+
+    <?php
+    if(isset($_GET['submit'])) {
+        $zoek = $_GET["zoek"];
+        $producten = zoekProduct($zoek);
+    }
+    else{
+        $producten = productenLijst(25);
+    }
+    ?>
+
+
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
+
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -6,35 +6,18 @@ include('include.php');
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+
 </head>
 <body>
 
-    <form method="get">
-        <input type="text" name="zoek" placeholder="Zoeken" />
-        <input type="submit" name="submit" />
-    </form>
 
-    <table>
-        <?php
-        if(isset($_GET['submit'])) {
-            $zoek = $_GET["zoek"];
-            $producten = zoekProduct($zoek);
-        }
-        else{
-            $producten = productenLijst(25);
-        }
-        ?>
-    </table>
 <div class="grid-container">
     <?php
-
-    foreach($producten as $product){
-        $id = $product["StockItemID"];
-//        <a class=\"nav-link\" href=\"gerechten/bon.php?id=$id\">Bon uitprinten</a>
-    print('<div class="grid-item" onclick="location.href=\'producten.php?id=' . $id . '\';">Product ' . $product["StockItemName"] . '</div>');
-    }
-     ?>
+        foreach($producten as $product){
+            $id = $product["StockItemID"];
+            print('<div class="grid-item" onclick="location.href=\'producten.php?id=' . $id . '\';">Product ' . $product["StockItemName"] . '</div>');
+        }
+    ?>
 </div>
 
 </body>
