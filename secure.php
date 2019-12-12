74cd4b4ee0e3011f8e99b474f38f828a597a23a2
duncan <S1140908@student.windesheim.nl>
2019-12-12 12:08:20 +0100

    WELKOM

--- secure.php
+++ secure.php
@@ -4,17 +4,17 @@ include('include.php');
 
 ?>
 <form method="post">
-    <h1><?php
+    <h1 class="registreren"><?php
     if(isset($_SESSION['gebruikersnaam'])){
-        print ("<br><br><br><br><br><br>u bent ingelogd als <br> ". $_SESSION['gebruikersnaam'] . "<br>" );
+        print ("<br><br><br><br><br>u bent ingelogd als <br> ". $_SESSION['gebruikersnaam'] . "<br> WELKOM" );
     ?></h1>
-    <td><input class="uiloggenbutton" type="submit" name="uitloggen" value="Uitloggen"></td>
-    <td><input class="registreerbutton" type="submit" name="homepage" value="terug naar homepage"></td>
+        <tr><td><input class="uiloggenbutton" type="submit" name="uitloggen" value="Uitloggen"</td></tr>
+        <tr><td><input class="registreerbutton" type="submit" name="homepage" value="terug naar homepage"></td></tr>
 </form>
 <?php
 if(isset($_POST['uitloggen'])){
     unset($_SESSION['gebruikersnaam']);
-    header('Location: inlog.php');
+    header('Location: index.php');
 }
 
 }
